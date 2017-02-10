209b19ee790446cd27128f4ac37ba49983c57333
Djenane Wail <Djenane Wail>
2017-02-10 11:47:26 +0100

    Fix find controller

---
+++ SppotiApi/src/main/java/com/fr/rest/controllers/search/FindTeamController.java
@@ -0,0 +1,64 @@
+package com.fr.rest.controllers.search;
+
+import com.fr.commons.dto.TeamResponse;
+import com.fr.rest.service.TeamControllerService;
+import com.fr.security.AccountUserDetails;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by wdjenane on 10/02/2017.
+ */
+@RestController
+@RequestMapping("/find/team")
+public class FindTeamController {
+
+    private Logger LOGGER = Logger.getLogger(FindTeamController.class);
+
+    private final TeamControllerService teamControllerService;
+
+    @Autowired
+    public FindTeamController(TeamControllerService teamControllerService) {
+        this.teamControllerService = teamControllerService;
+    }
+
+    /**
+     * @param team
+     * @param page
+     * @param authentication
+     * @return All found teams containing the String (team).
+     */
+    @GetMapping("/team/{team}/{page}")
+    public ResponseEntity<List<TeamResponse>> findTeam(@PathVariable String team, @PathVariable int page, Authentication authentication) {
+
+        AccountUserDetails accountUserDetails = (AccountUserDetails) authentication.getPrincipal();
+
+        List<TeamResponse> teamResponses;
+        try {
+
+            teamResponses = teamControllerService.findAllTeams(team, accountUserDetails.getUuid(), page);
+
+            if (teamResponses.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+            }
+
+        } catch (RuntimeException e) {
+            LOGGER.error("Find All teams error: " + e);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        }
+
+        return new ResponseEntity<>(teamResponses, HttpStatus.OK);
+    }
+
+}
