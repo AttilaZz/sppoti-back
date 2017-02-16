402f685a2ed3b2e9287af139cc92311fe842ff17
Djenane Wail <Djenane Wail>
2017-02-16 11:02:57 +0100

    sppoti utils

---
+++ SppotiCommons/src/main/java/com/fr/commons/utils/SppotiUtils.java
@@ -0,0 +1,54 @@
+package com.fr.commons.utils;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.security.crypto.codec.Base64;
+/**
+ * Created by wdjenane on 16/02/2017.
+ */
+public class SppotiUtils
+{
+    /**
+     * Coder une chaine de caractères en 64 bit
+     *
+     * @param content
+     * @return une chaine de caractère.
+     */
+    public static String convertTo64(final String content)
+    {
+        final byte[] encodedBytes = Base64.encode(content.getBytes());
+        return new String(encodedBytes);
+    }
+
+    /**
+     * Décoder une chaine de caractères codée en 64bit
+     *
+     * @param content
+     * @return une chaine de caractère.
+     */
+    public static String decode64ToString(final String content)
+    {
+        final byte[] decodedBytes = Base64.decode(content.getBytes());
+        return new String(decodedBytes);
+    }
+
+    /**
+     * Crée un Long aléatoire.
+     *
+     * @param scale nombre de chiffre
+     * @return un Long random
+     */
+    public static Long randomLong(final int scale)
+    {
+        return new Integer(randomString(scale).hashCode()).longValue();
+    }
+
+    /**
+     * Crée un String aléatoire.
+     *
+     * @param size nombre de caractère
+     * @return un String random
+     */
+    public static String randomString(final int size)
+    {
+        return RandomStringUtils.randomAlphanumeric(size);
+    }
+}
