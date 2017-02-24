eda0ae30c4ba65c506409b08cb09e125cdc09b1b
Djenane Wail <Djenane Wail>
2017-02-24 10:21:10 +0100

    fix sql file

--- src/sql/init_database.sql
+++ src/sql/init_database.sql
@@ -6,7 +6,7 @@ FIRST USER
  */
 INSERT INTO sppoti.user (id, uuid, version, confirmation_code, confirmed, date_born, deleted, description, email, first_name, job, last_name, password, sexe, telephone, username)
 VALUES
-  (11, 12345678, 1, 'code', TRUE, curdate(), FALSE, NULL, 'wail.djenane@gmail.com', 'wail', NULL, 'djenane', 'dje123',
+  (11, 12345678, 1, 'code', TRUE, curdate(), FALSE, NULL, 'wail.djenane@gmail.com', 'wail', NULL, 'djenane', 'piratusse',
    'H', NULL, 'piratusse');
 
 INSERT INTO sppoti.user_roles (users_id, roles_id) VALUES (11, 1);
@@ -18,7 +18,7 @@ SECOND USER
  */
 INSERT INTO sppoti.user (id, uuid, version, confirmation_code, confirmed, date_born, deleted, description, email, first_name, job, last_name, password, sexe, telephone, username)
 VALUES (12, 12377778, 2, 'code1', TRUE, curdate(), FALSE, NULL, 'wail.djenane@gmail.co', 'bachir', NULL, 'bouacheria',
-        'dje123', 'H', NULL, 'bachir31');
+        'bachir31', 'H', NULL, 'bachir31');
 
 INSERT INTO sppoti.user_roles (users_id, roles_id) VALUES (12, 1);
 INSERT INTO sppoti.user_related_sports (subscribed_users_id, related_sports_id) VALUES (12, 2);
@@ -30,7 +30,7 @@ THIRD USER
  */
 INSERT INTO sppoti.user (id, uuid, version, confirmation_code, confirmed, date_born, deleted, description, email, first_name, job, last_name, password, sexe, telephone, username)
 VALUES
-  (13, 12999678, 3, 'code2', TRUE, curdate(), FALSE, NULL, 'wail.djenane@gmail.c', 'amar', NULL, 'sebaa', 'dje123', 'H',
+  (13, 12999678, 3, 'code2', TRUE, curdate(), FALSE, NULL, 'wail.djenane@gmail.c', 'amar', NULL, 'sebaa', 'amar31', 'H',
    NULL, 'amar31');
 
 INSERT INTO sppoti.user_roles (users_id, roles_id) VALUES (13, 1);
