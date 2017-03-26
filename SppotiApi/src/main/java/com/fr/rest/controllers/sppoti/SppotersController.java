97a9dce07b1c5827b0822d198b2c4773cad61a52
DzCorps <wail.djenane@google.com>
2017-03-26 04:09:53 +0200

    clean some portion of code

--- SppotiApi/src/main/java/com/fr/rest/controllers/sppoti/SppotersController.java
+++ SppotiApi/src/main/java/com/fr/rest/controllers/sppoti/SppotersController.java
@@ -23,50 +23,30 @@ public class SppotersController {
     public void setSppotiControllerService(SppotiControllerService sppotiControllerService) {
         this.sppotiControllerService = sppotiControllerService;
     }
-
-    private Logger LOGGER = Logger.getLogger(SppotiAddController.class);
-
+    
     /**
-     *
      * @param sppotiId sppoti id.
-     * @param userId user id.
+     * @param userId   user id.
      * @return 202 status if sppoti status updated.
      */
     @PutMapping("/accept/{userId}")
     public ResponseEntity<String> acceptSppoti(@PathVariable int sppotiId, @PathVariable int userId) {
 
-        try {
-
-            sppotiControllerService.acceptSppoti(sppotiId, userId);
-
-        } catch (RuntimeException e) {
-            LOGGER.error("Accept sppoti error: ", e);
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-
-        }
+        sppotiControllerService.acceptSppoti(sppotiId, userId);
 
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
 
     }
 
     /**
-     *
      * @param sppotiId sppoti id.
-     * @param userId user id.
+     * @param userId   user id.
      * @return 202 status if sppoti status updated.
      */
     @PutMapping("/refuse/{userId}")
     public ResponseEntity<Void> refuseSppoti(@PathVariable int sppotiId, @PathVariable int userId) {
 
-        try {
-
-            sppotiControllerService.refuseSppoti(userId, sppotiId);
-
-        } catch (RuntimeException e) {
-            LOGGER.error("Accept sppoti error: ", e);
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-
-        }
+        sppotiControllerService.refuseSppoti(userId, sppotiId);
 
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
 
