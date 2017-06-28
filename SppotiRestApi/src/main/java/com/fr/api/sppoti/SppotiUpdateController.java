01e8f8d5d2feb0f484a9370c45c788af194e139c
wdjenane <djenane_wail@yahoo.fr>
2017-06-28 17:44:26 +0200

    clean code

--- SppotiRestApi/src/main/java/com/fr/api/sppoti/SppotiUpdateController.java
+++ SppotiRestApi/src/main/java/com/fr/api/sppoti/SppotiUpdateController.java
@@ -47,11 +47,8 @@ class SppotiUpdateController
 	ResponseEntity<SppotiDTO> updateSppoti(@PathVariable final String sppotiId, @RequestBody final SppotiDTO sppotiRequest,
 										   final Authentication authentication)
 	{
-		
-		final AccountUserDetails accountUserDetails = (AccountUserDetails) authentication.getPrincipal();
-		
 		//throws exception if user is not the sppoti admin
-		if (!this.sppotiControllerService.isSppotiAdmin(sppotiId, accountUserDetails.getId())) {
+		if (!this.sppotiControllerService.isSppotiAdmin(sppotiId)) {
 			throw new NotAdminException("You must be the sppoti admin to continue");
 		}
 		
