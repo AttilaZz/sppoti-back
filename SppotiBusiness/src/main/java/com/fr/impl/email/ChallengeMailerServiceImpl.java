276090024b3c4f5c0cb0d07fd57235ef74598205
DzCorps <wail.djenane@google.com>
2017-12-29 14:49:39 +0100

    Add challenge mailing properties

--- SppotiBusiness/src/main/java/com/fr/impl/email/ChallengeMailerServiceImpl.java
+++ SppotiBusiness/src/main/java/com/fr/impl/email/ChallengeMailerServiceImpl.java
@@ -3,19 +3,23 @@ package com.fr.impl.email;
 import com.fr.commons.dto.MailResourceContent;
 import com.fr.entities.SppotiEntity;
 import com.fr.entities.TeamEntity;
+import com.fr.entities.TeamMemberEntity;
 import com.fr.entities.UserEntity;
+import com.fr.repositories.TeamRepository;
 import com.fr.service.UserParamService;
 import com.fr.service.email.ChallengeMailerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 /**
  * Created by djenanewail on 12/29/17.
@@ -29,13 +33,26 @@ public class ChallengeMailerServiceImpl extends ApplicationMailerServiceImpl imp
 	
 	@Autowired
 	private TemplateEngine templateEngine;
-	
+	@Autowired
+	private TeamRepository teamRepository;
 	@Autowired
 	private UserParamService userParamService;
 	
 	@Override
 	public void onSendChallenge(final TeamEntity from, final TeamEntity to, final SppotiEntity sppoti) {
 		this.LOGGER.info("Sending email on creating a challenge ..");
+		final List<TeamMemberEntity> teamToAdmin = to.getTeamMembers().stream()
+				.filter(m -> m.getAdmin() && this.userParamService.canReceiveEmail(m.getUser().getUuid()))
+				.collect(Collectors.toList());
+		
+		if (CollectionUtils.isEmpty(teamToAdmin)) {
+			this.LOGGER.info("All team admin have deactivated mailing");
+			return;
+		}
+		
+		teamToAdmin.forEach(m -> {
+			//send mail
+		});
 	}
 	
 	@Override
@@ -47,7 +64,18 @@ public class ChallengeMailerServiceImpl extends ApplicationMailerServiceImpl imp
 	@Override
 	public void onRefuseChallenge(final TeamEntity from, final TeamEntity to, final SppotiEntity sppoti) {
 		this.LOGGER.info("Sending email on refusing a challenge ..");
+		final List<TeamMemberEntity> teamToAdmin = to.getTeamMembers().stream()
+				.filter(m -> m.getAdmin() && this.userParamService.canReceiveEmail(m.getUser().getUuid()))
+				.collect(Collectors.toList());
 		
+		if (CollectionUtils.isEmpty(teamToAdmin)) {
+			this.LOGGER.info("All team admin have deactivated mailing");
+			return;
+		}
+		
+		teamToAdmin.forEach(m -> {
+			//send mail
+		});
 	}
 	
 	private void prepareAndSendEmail(final String subject, final String content, final UserEntity to,
